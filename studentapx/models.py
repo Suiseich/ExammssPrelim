a72438ce23782a605640706301fd601fbe002b23
Suiseich <aarontayag89@gmail.com>
2025-09-10 10:21:07 +0800

    Unang commit

--- studentapx/models.py
+++ studentapx/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class StudentRecord(models.Model):
-    student_id = models.CharField(max_length=20)
+    student_id = models.CharField(max_length=30)
     full_name = models.CharField(max_length=100)
     course = models.CharField(max_length=50)
     year_level = models.IntegerField()
